72555a812e646cddb0a3a362746d0960d68644d5
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-08-07 14:58:05 +0530

    all codes added

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+var cors = require("cors");
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.post("/analyze", async (req, res) => {
+  const { url } = req.body;
+  console.log(url);
+
+  if (!url) {
+    return res.status(400).json({ error: " URL is required" });
+  }
+
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const performanceTiming = JSON.parse(await page.evaluate(() => JSON.stringify(window.performance.timing)));
+
+    await browser.close();
+
+    const metrics = {
+      pageLoadTime: performanceTiming.loadEventEnd - performanceTiming.navigationStart,
+      totalRequestSize: 0,
+      numberOfRequests: 0,
+    };
+
+    res.json({data: metrics , msg: "🥳Website analyzed successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Error analyzing the website" });
+  }
+});
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import "./App.css";
+import { FaSquareGithub, FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+
+const App = () => {
+  const [url, setUrl] = useState("");
+  const [metrics, setMetrics] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+    setMetrics(null);
+    const toastId = toast.loading("Please Wait...");
+    try {
+      const res = await axios.post("http://localhost:5000/analyze", { url });
+      setMetrics(res.data.data);
+      toast.success(res.data.msg, { id: toastId });
+    } catch (error) {
+      setError("Error analyzing the website. Please try again.");
+      toast.error(error.response.data.error, { id: toastId });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className='app'>
+      <div className='header'>
+        <a href=''>
+          <FaSquareGithub style={{ transform: "translateY(5px)" }} />
+        </a>
+        <a href=''>
+          <FaLinkedin style={{ transform: "translateY(5px)" }} />
+        </a>
+        <a href=''>
+          <FaSquareXTwitter style={{ transform: "translateY(5px)" }} />
+        </a>
+      </div>
+      <h1 className='title'>SpeedX - Website Performance Analyzer</h1>
+      <form className='form' onSubmit={handleSubmit}>
+        <input type='text' value={url} onChange={(e) => setUrl(e.target.value)} placeholder='Enter website URL' className='input' />
+        <button type='submit' className='button' disabled={loading} style={{ cursor: loading ? "wait" : "pointer" }}>
+          {loading ? "Analyzing..." : "Analyze"}
+        </button>
+      </form>
+      {error && <p className='error'>{error}</p>}
+      {metrics && (
+        <div className='metrics-card'>
+          <h2>Performance Metrics</h2>
+          <p>
+            <strong>Page Load Time:</strong> {metrics.pageLoadTime} ms
+          </p>
+          <p>
+            <strong>Total Request Size:</strong> {metrics.totalRequestSize} KB
+          </p>
+          <p>
+            <strong>Number of Requests:</strong> {metrics.numberOfRequests}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
